d10c55211df446043a6a69ece2b2b05fc5c87e28
admin1212b <myesetnod3223@gmail.com>
2023-07-17 14:44:28 +0500

    Anitial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/card";
+import movies from "./mocks/movies";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">Movies list</div>
+      <div className="cards">
+        {movies.map((movie, index) => {
+          return <Card key={index} props={movie} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,16 @@
+const Card = ({ props }) => {
+  const { name, liked, src, year } = props;
+  return (
+    <div className="card">
+      <div className="top">
+        <h2>{name}</h2>
+        <img src={src} alt={name} />
+      </div>
+      <div className="bottom">
+        <p>♥ {liked}</p>
+        <p>{year}</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
